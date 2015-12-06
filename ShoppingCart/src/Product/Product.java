feddd909694f74a6f1dbc5ae8c67cf646d761879
Ricardo Solon <ricky0604@gmail.com>
2015-12-06 15:06:37 -0500

    ShoppingCart to test promotions

---
+++ ShoppingCart/src/Product/Product.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Product;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class Product {
+    
+    private String name;
+    private String ID;
+    private double price;
+    private String category;
+    
+    public Product(){}
+
+    public Product(String name, String ID, double price, String category) {
+        this.name = name;
+        this.ID = ID;
+        this.price = price;
+        this.category = category;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Product other = (Product) obj;
+        if (!Objects.equals(this.ID, other.ID)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    
+    
+    
+    
+}
